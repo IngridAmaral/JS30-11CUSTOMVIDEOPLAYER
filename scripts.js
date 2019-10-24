1160eed545f832ca672d127f82981809a1bafe5c
IngridAmaral <52465213+IngridAmaral@users.noreply.github.com>
2019-10-24 15:47:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,9 +7,31 @@ const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 function togglePlay(){
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
+    const method = video.paused? 'play' : 'pause';
+    video[method]();  
+    /* OR
+    if (video.paused) { video.play(); }
+    else { video.pause(); }*/
 }
+
+function buttonUpdate(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip) /*returns a number*/
+}
+
+function hangleRangeUpdate(){
+    video[this.name] = this.value;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', buttonUpdate);
+video.addEventListener('pause', buttonUpdate);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(skipBtn => skipBtn.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', hangleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', hangleRangeUpdate));
